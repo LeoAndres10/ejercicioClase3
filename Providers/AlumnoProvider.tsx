e8a07aaf7640926e0af3403cccb5dd906db9a722
LeoAndres10 <leo1318velasquez@hotmail.es>
2025-08-05 23:26:23 -0600

    10 registros

--- Providers/AlumnoProvider.tsx
+++ Providers/AlumnoProvider.tsx
@@ -6,7 +6,10 @@ import { ContextAlumno } from "../Contexto/ContextAlumno";
 
 export default function AlumnoProvider({children}: Plantilla){
 
-    const [listaAlumnos,setListaAlumnos ]= useState<Alumno[]>([]);
+    const [listaAlumnos,setListaAlumnos ]= useState<Alumno[]>([{'id':1,'nombre':'Juan'},
+        {'id':2,'nombre':'Jose'},{'id':3,'nombre':'Pedro'},{'id':4,'nombre':'Ismael'},
+        {'id':5,'nombre':'Ramon'},{'id':6,'nombre':'Jose'},{'id':7,'nombre':'Victor'},{'id':8,'nombre':'Ariel'},
+        {'id':9,'nombre':'Andres'},{'id':10,'nombre':'Arturo'}]);
 
     function agregarListaAlumno(alumnoObjeto: Alumno){
         setListaAlumnos([...listaAlumnos,alumnoObjeto]);
